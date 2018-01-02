4537d5ee420972c48ad7ed107e91ed56ee157d88
SY Young <r38411@yahoo.com>
2018-01-02 18:15:55 +0800

    finish part1-DictionaryBST

--- DictionaryBST.java
+++ DictionaryBST.java
@@ -12,6 +12,9 @@ public class DictionaryBST implements Dictionary
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
+   public DictionaryBST() {
+	   dict = new TreeSet<String>();
+   }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -21,7 +24,11 @@ public class DictionaryBST implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
-        return false;
+    		String newWord = word.toLowerCase();
+    		if (!dict.contains(newWord)) {
+    			dict.add(newWord);
+    		}
+        return true;
     }
 
 
@@ -29,13 +36,14 @@ public class DictionaryBST implements Dictionary
     public int size()
     {
     	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
     	//TODO: Implement this method
-        return false;
+    		String newWord = s.toLowerCase();
+        return dict.contains(newWord);
     }
 
 }
